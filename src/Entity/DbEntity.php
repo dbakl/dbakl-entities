71b6996d9f3254f0d7eba6374248ca9dbd03e54d
matthes <leuffen@continue.de>
2016-11-30 16:09:06 +0100

    Initial

---
+++ src/Entity/DbEntity.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: matthes
+     * Date: 30.11.16
+     * Time: 16:01
+     */
+
+    namespace DbAkl\Entity;
+
+
+    trait DbEntity
+    {
+
+
+
+    }
